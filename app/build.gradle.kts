62a45e3347a484194a2f0eff071031181542d2f4
shusbssjsijhsis <shusbssjsijhsis@users.noreply.github.com>
2026-01-17 19:12:10 +0000

    fix: add compose compiler plugin for Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
-plugins {
-  alias(libs.plugins.android.application)
+plugins {alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.compose.plugin)
 }
 
 android {
